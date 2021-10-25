0449e7d15f8d11474ea2cac2df85cb88868a0cf0
Rafael Espinal <respinal1200@gmail.com>
2021-10-25 03:42:13 -0400

    Componentes creados

--- InventarioAlmacen/Dashboard.cs
+++ InventarioAlmacen/Dashboard.cs
@@ -16,5 +16,46 @@ namespace InventarioAlmacen
         {
             InitializeComponent();
         }
+
+        private void btnMant_Click(object sender, EventArgs e)
+        {
+            subMant.Visible = true;
+        }
+        void abrirForm(object form)
+        {
+            if (this.panelContenedor.Controls.Count > 0)
+                this.panelContenedor.Controls.RemoveAt(0);
+            Form f = form as Form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill;
+            this.panelContenedor.Controls.Add(f);
+            this.panelContenedor.Tag = f;
+            f.Show();
+        }
+
+
+        private void panelContenedor_Click(object sender, EventArgs e)
+        {
+            subMant.Visible = false;
+        }
+
+        private void btnMProd_Click(object sender, EventArgs e)
+        {
+            subMant.Visible = false;
+            abrirForm(new Productos());
+        }
+
+        private void btnMUser_Click(object sender, EventArgs e)
+        {
+            subMant.Visible = false;
+            abrirForm(new Usuarios());
+
+        }
+
+        private void btnMMar_Click(object sender, EventArgs e)
+        {
+            subMant.Visible = false;
+            abrirForm(new Marcas());
+        }
     }
 }
